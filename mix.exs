eb8b7fa9e62f8774bf8e95b226d7e9167b693dde
Milton Mazzarri <milmazz@gmail.com>
2015-08-27 22:49:25 +0000

    Add a Universally unique identifier w/dc:identifier

--- mix.exs
+++ mix.exs
@@ -28,6 +28,7 @@ defmodule ExDocEPUB.Mixfile do
   # Type `mix help deps` for more examples and options
   defp deps do
     [
+      {:uuid, "~> 1.0"},
       {:earmark, "~> 0.1.17"},
       {:ex_doc, "~> 0.8"}
     ]
