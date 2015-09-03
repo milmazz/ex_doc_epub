d4b60e3f9b83be3efc7907ef4beef6a492602283
Milton Mazzarri <milmazz@gmail.com>
2015-09-02 23:46:41 -0500

    Borrow fixtures and helpers from ExDoc

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,9 @@
 ExUnit.start()
+
+if Enum.empty? Path.wildcard("test/tmp/ebin/*.beam") do
+  IO.puts "Compiling fixtures..."
+  {result, _error_code} = System.cmd "elixirc", ["test/fixtures", "-o", "test/tmp/ebin"], stderr_to_stdout: true
+  IO.puts result
+end
+
+Code.prepend_path "test/tmp/ebin"
