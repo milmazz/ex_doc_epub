b9451afdcb7f59b7d156e12b2e8435efe4661cf8
Milton Mazzarri <milmazz@gmail.com>
2016-11-07 16:57:25 -0600

    Fix reference to README.md

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule ExDocEPUB.Mixfile do
   use Mix.Project
 
-  @version "0.0.2"
+  @version "0.0.3"
 
   def project do
     [app: :ex_doc_epub,
@@ -13,31 +13,17 @@ defmodule ExDocEPUB.Mixfile do
 
      # Hex.pm
      description: description,
-     package: package,
+     package: package(),
 
      # Docs
      name: "ExDocEPUB",
-     docs: [readme: "README.md",
-            source_ref: "v#{@version}", main: "ExDocEPUB.Formatter.EPUB",
-            source_url: "https://github.com/milmazz/ex_doc_epub"]]
+     docs: docs()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     [{:earmark, "~> 0.1.17", optional: true},
       {:ex_doc, "~> 0.9"}]
@@ -54,5 +40,11 @@ defmodule ExDocEPUB.Mixfile do
      licenses: ["MIT"],
      links: %{"GitHub" => "https://github.com/milmazz/ex_doc_epub",
               "Docs" => "http://hexdocs.pm/ex_doc_epub/#{@version}"}]
+  end
+
+  defp docs do
+    [readme: "readme.md",
+     source_ref: "v#{@version}", main: "ExDocEPUB.Formatter.EPUB",
+     source_url: "https://github.com/milmazz/ex_doc_epub"]
   end
 end
